5a95e3c7348cebeeaf7ffaaf124e408bcfcc5eb1
Nakul garg <nakulgarg2000@gmail.com>
2020-07-08 16:54:02 +0530

    Tab's Working

--- jamiee_flutter/lib/screens/homePage.dart
+++ jamiee_flutter/lib/screens/homePage.dart
@@ -3,7 +3,9 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:jamiee_flutter/screens/loginPage.dart';
+import 'package:jamiee_flutter/screens/signupPage.dart';
 import 'package:jamiee_flutter/styles/colors.dart';
+import 'package:jamiee_flutter/styles/text.dart';
 
 class HomePagePlatform extends StatelessWidget {
   @override
@@ -27,35 +29,104 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Widget _child;
+  PageType _pageType;
+  @override
+  void initState() {
+    _child = LoginPage();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    print("++++++++++HOME PAGE BUILD+++++++++++");
+    Color _loginColor;
+    Color _signupColor;
+    switch (_pageType) {
+      case PageType.LoginPage:
+        _loginColor = AppColors.primaryBlue;
+        _signupColor = AppColors.grayLoginTabColor;
+        break;
+      case PageType.SignupPage:
+        _signupColor = AppColors.primaryBlue;
+        _loginColor = AppColors.grayLoginTabColor;
+        break;
+      default:
+        _loginColor = AppColors.primaryBlue;
+        _signupColor = AppColors.grayLoginTabColor;
+    }
     return Container(
       padding: EdgeInsets.symmetric(vertical: 35.0, horizontal: 30.0),
       child: Column(
         children: <Widget>[
+          SizedBox(
+            height: 100.0,
+          ),
           Row(
             children: <Widget>[
               InkWell(
-                onTap: () => print("F"),
+                onTap: () {
+                  setState(() {
+                    _child = LoginPage();
+                    _pageType = PageType.LoginPage;
+                  });
+                },
                 child: Container(
-                  width: (MediaQuery.of(context).size.width - 60.0) / 2,
-                  child: Text("Sign Up"),
-                ),
+                    height: 50.0,
+                    width: (MediaQuery.of(context).size.width - 60.0) / 2,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        Text(
+                          "Login",
+                          style: AppTextStyle.tabText(_loginColor),
+                        ),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        Container(
+                          color: _loginColor,
+                          height: 5.0,
+                        )
+                      ],
+                    )),
               ),
               InkWell(
-                onTap: () => print("F"),
+                onTap: () {
+                  setState(() {
+                    _child = SignupPage();
+                    _pageType = PageType.SignupPage;
+                  });
+                },
                 child: Container(
-                  width: (MediaQuery.of(context).size.width - 60) / 2,
-                  child: Text("Login"),
-                ),
+                    height: 50.0,
+                    width: (MediaQuery.of(context).size.width - 60.0) / 2,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        Text(
+                          "Sign Up",
+                          style: AppTextStyle.tabText(_signupColor),
+                        ),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        Container(
+                          color: _signupColor,
+                          height: 5.0,
+                        )
+                      ],
+                    )),
               ),
             ],
           ),
           Expanded(
-            child: LoginPage(),
+            child: _child,
           )
         ],
       ),
     );
   }
 }
+
+enum PageType { LoginPage, SignupPage }
--- jamiee_flutter/lib/styles/text.dart
+++ jamiee_flutter/lib/styles/text.dart
@@ -4,6 +4,18 @@ import 'package:google_fonts/google_fonts.dart';
 import 'colors.dart';
 
 class AppTextStyle {
-  static TextStyle get loginButtonText =>
-      GoogleFonts.spaceMono(textStyle: TextStyle(color: AppColors.white));
+  static TextStyle get loginButtonText => GoogleFonts.spaceMono(
+      fontSize: 20.0,
+      fontWeight: FontWeight.w600,
+      textStyle: TextStyle(color: AppColors.white));
+
+  static TextStyle tabText(Color _color) {
+    return GoogleFonts.spaceMono(
+      fontWeight: FontWeight.w900,
+      textStyle: TextStyle(
+        fontSize: 20.0,
+        color: _color,
+      ),
+    );
+  }
 }
