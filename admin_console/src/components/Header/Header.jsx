68aa6053e2480f81854a07f4b6fd9699610b400b
Paritosh Batish <batishparitosh2@gmail.com>
2020-07-13 02:36:37 +0530

    Completed styling and functioning of header Dropdown onblur left

--- admin_console/src/components/Header/Header.jsx
+++ admin_console/src/components/Header/Header.jsx
@@ -2,6 +2,24 @@ import React, { Component } from "react";
 import "./Header.css";
 
 export default class Header extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      avatar_menu_open: false,
+    };
+  }
+
+  toggle_avatar_menu = () => {
+    this.setState({
+      avatar_menu_open: !this.state.avatar_menu_open,
+    });
+  };
+
+  check_outstide_click = (event) => {
+    console.log(event.target);
+  };
+
   render() {
     return (
       <div className="Header">
@@ -9,14 +27,30 @@ export default class Header extends Component {
           <span className="material-icons">menu</span>
         </div>
         <div className="searchbar">
+          <input type="text" className="searchbar-input" placeholder="Search" />
           <span className="material-icons search-icon">search</span>
-          <span className="placeholder-text">Search</span>
         </div>
         <div className="account-info">
           <div className="name">Paritosh Batish</div>
           <div className="admin">Admin</div>
         </div>
-        <div className="avatar">P</div>
+        <div className="avatar" onClick={this.toggle_avatar_menu}>
+          P
+        </div>
+
+        <div
+          className={
+            this.state.avatar_menu_open
+              ? "avatar-menu avatar-menu-active"
+              : "avatar-menu"
+          }
+          onBlur={(event) => this.check_outstide_click(event)}
+        >
+          <ul>
+            <li>Account Info</li>
+            <li>Logout</li>
+          </ul>
+        </div>
       </div>
     );
   }
