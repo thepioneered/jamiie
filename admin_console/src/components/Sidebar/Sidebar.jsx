f13f9f9df7f8525772f675143c57b56f22948cf6
Paritosh Batish <batishparitosh2@gmail.com>
2020-07-12 19:22:14 +0530

    Sidebar v0.7

--- admin_console/src/components/Sidebar/Sidebar.jsx
+++ admin_console/src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,39 @@
 import React, { Component } from "react";
 import "./Sidebar.css";
+import logo from "../../Only Logo.svg";
 
 export default class Sidebar extends Component {
   render() {
     return (
       <div className="Sidebar">
-        <ul>
-          <li>Dashboard</li>
-          <li>Transactions</li>
-          <li>Users</li>
-          <li>Groups</li>
+        <div className="logo-wrapper">
+          <img src={logo} className="logo" alt="logo" />
+        </div>
+        <ul className="index">
+          <li className="active">
+            <div className="index-icon">
+              <span className="material-icons">dashboard</span>
+            </div>
+            <div className="index-text">Dashboard</div>
+          </li>
+          <li>
+            <div className="index-icon">
+              <span className="material-icons">timeline</span>
+            </div>
+            <div className="index-text">Transactions</div>
+          </li>
+          <li>
+            <div className="index-icon">
+              <span className="material-icons">group</span>
+            </div>
+            <div className="index-text">Users</div>
+          </li>
+          <li>
+            <div className="index-icon">
+              <span className="material-icons">person_outline</span>
+            </div>
+            <div className="index-text">Groups</div>
+          </li>
         </ul>
       </div>
     );
