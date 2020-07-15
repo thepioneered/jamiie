f62b9a2af00e94f92c3bce829dd09215ad92f661
Paritosh Batish <batishparitosh2@gmail.com>
2020-07-15 22:54:17 +0530

    Searching Done

--- admin_console/src/components/Header/Header.jsx
+++ admin_console/src/components/Header/Header.jsx
@@ -8,13 +8,39 @@ export default class Header extends Component {
 
     this.state = {
       avatar_menu_open: false,
+      search_focused: false,
     };
   }
 
-  toggle_avatar_menu = () => {
+  ToggleAvatarMenu = () => {
     this.setState({
       avatar_menu_open: !this.state.avatar_menu_open,
     });
+  };
+
+  ToggleSearchHelp = (by) => {
+    this.setState({
+      search_focused: !this.state.search_focused,
+    });
+    console.log("ToggleSearchHelp called by", by);
+  };
+
+  FilterSearch = (event) => {
+    const filter = event.target.value.toUpperCase();
+    const a = document.getElementsByTagName("a");
+    for (let i = 0; i < a.length; i++) {
+      let txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+  };
+
+  DropdownSelect = (event) => {
+    // console.log(event.target.value);
+    console.log("DropdownSelect");
   };
 
   render() {
@@ -24,18 +50,39 @@ export default class Header extends Component {
           <span className="material-icons">menu</span>
         </div>
         <div className="searchbar">
-          <input type="text" className="searchbar-input" placeholder="Search" />
+          <input
+            type="text"
+            className="searchbar-input"
+            placeholder="Search"
+            onFocus={() => this.ToggleSearchHelp("focus")}
+            onBlur={() => this.ToggleSearchHelp("blur")}
+            onChange={(event) => this.FilterSearch(event)}
+          />
+          <div
+            className="search-content"
+            style={{
+              display: this.state.search_focused ? null : "none",
+            }}
+          >
+            <a href="#about">About</a>
+            <a href="#base">Base</a>
+            <a href="#blog">Blog</a>
+            <a href="#contact">Contact</a>
+            <a href="#custom">Custom</a>
+            <a href="#support">Support</a>
+            <a href="#tools">Tools</a>
+          </div>
           <span className="material-icons search-icon">search</span>
         </div>
         <div className="account-info">
           <div className="name">Paritosh Batish</div>
           <div className="admin">Admin</div>
         </div>
-        <div className="avatar" onClick={this.toggle_avatar_menu}>
+        <div className="avatar" onClick={this.ToggleAvatarMenu}>
           P
         </div>
         {this.state.avatar_menu_open ? (
-          <AvatarMenu toggle_avatar_menu={this.toggle_avatar_menu} />
+          <AvatarMenu toggle_avatar_menu={this.ToggleAvatarMenu} />
         ) : null}
       </div>
     );
