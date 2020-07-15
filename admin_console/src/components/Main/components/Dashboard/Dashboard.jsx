905c35db01ecd0c68f0f86bd3e60cf0b2d3b5c07
Paritosh Batish <batishparitosh2@gmail.com>
2020-07-15 20:13:36 +0530

    Dynamic Cards

--- admin_console/src/components/Main/components/Dashboard/Dashboard.jsx
+++ admin_console/src/components/Main/components/Dashboard/Dashboard.jsx
@@ -2,17 +2,37 @@ import React, { Component } from "react";
 import "./Dashboard.css";
 
 export default class Dashboard extends Component {
-  getCards = () => {
-    let x = 46289;
-    const y = x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  getCards = (name, number, percentage) => {
+    const addCommasToNumber = number
+      .toString()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+    let color = "#39dc00";
+    let status = "Up";
+    let icon = "trending_up";
+
+    if (percentage === 0) {
+      color = "#a3a3a3";
+      status = "Change";
+      icon = "linear_scale";
+    } else if (percentage < 0) {
+      color = "#ea3a3a";
+      status = "Down";
+      icon = "trending_down";
+    }
+
     return (
       <div className="card">
-        <div className="card-heading">Total Transactions</div>
-        <div className="card-number">{y}</div>
+        <div className="card-heading">{name}</div>
+        <div className="card-number">{addCommasToNumber}</div>
         <div className="card-description">
-          <span className="material-icons card-icon">trending_up</span>
-          <span className="card-percentage">&nbsp;2.4%</span>
-          <span className="card-status">&nbsp;Up from Yesterday</span>
+          <span className="material-icons card-icon" style={{ color: color }}>
+            {icon}
+          </span>
+          <span className="card-percentage" style={{ color: color }}>
+            &nbsp;{Math.abs(percentage)}%
+          </span>
+          <span className="card-status">&nbsp;{status} from Yesterday</span>
         </div>
       </div>
     );
@@ -22,10 +42,10 @@ export default class Dashboard extends Component {
       <div className="dashboard">
         <div className="card-flex-wrapper">
           <div className="card-container">
-            {this.getCards()}
-            {this.getCards()}
-            {this.getCards()}
-            {this.getCards()}
+            {this.getCards("Total Transactions", 46289, 2.4)}
+            {this.getCards("Total Users", 22544, 0.4)}
+            {this.getCards("Total Groups", 11289, -1.4)}
+            {this.getCards("Completed Pools", 33289, 0)}
           </div>
         </div>
       </div>
