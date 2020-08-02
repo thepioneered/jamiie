b6a7717d20de33d5c6de05ed1b936a4e49b27288
Nitish Sharma <nitishrkt@gmail.com>
2020-08-02 12:58:07 +0530

    added host

--- practiceapi/settings.py
+++ practiceapi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ojsw$$&dge176!tu(*52*z5xxkt76p03wj20o!&mr=3qepe#6m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
