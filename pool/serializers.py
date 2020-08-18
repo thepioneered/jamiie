915f730c1202e8922127049394c520c6a9b8087b
Nitish Sharma <nitishrkt@gmail.com>
2020-08-18 16:56:26 +0530

    serializer removed

--- pool/serializers.py
+++ pool/serializers.py
@@ -1,11 +1,3 @@
 from rest_framework import serializers
 from .models import *
 #create your serializers here 
-class SearchPoolSeriailzier(serializers.Serializer):
-    poolid = models.CharField()
-    poolname = models.CharField()
-    poolowner = models.ForeignKey(User,on_delete=models.CASCADE)
-    poolamount = models.FloatField()
-    maxmember = models.IntegerField()
-    joinedmember = models.IntegerField()
-    deadline = models.CharField()
