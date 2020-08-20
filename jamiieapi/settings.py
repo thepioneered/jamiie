0b96d530b38e4a65e5bb6bfb7de37dd2d79c276f
Nitish Sharma <nitishrkt@gmail.com>
2020-08-20 20:58:19 +0530

    testing

--- jamiieapi/settings.py
+++ jamiieapi/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 from dotenv import load_dotenv
 from pathlib import Path
-import os
 env = Path('.') / '.env'
 load_dotenv(dotenv_path=env)
 
