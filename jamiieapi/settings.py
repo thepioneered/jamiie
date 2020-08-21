b67d77f4eaa87f3fe8eec024fefbc6c1dde332a0
Nitish Sharma <nitishrkt@gmail.com>
2020-08-21 11:27:04 +0530

    postgresql integrated

--- jamiieapi/settings.py
+++ jamiieapi/settings.py
@@ -97,8 +97,12 @@ WSGI_APPLICATION = 'jamiieapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('NAME'),
+        'HOST': os.getenv('HOST'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'PORT': os.getenv('PORT'),
     }
 }
 
