a4967b2cf4aff4ceaf075c8864612667700cb303
Paritosh Batish <batishparitosh2@gmail.com>
2020-11-11 13:04:19 +0530

    Redeployed

--- admin_console/server.js
+++ admin_console/server.js
@@ -18,6 +18,6 @@ app.prepare().then(() => {
     handle(req, res, parsedUrl);
   }).listen(3000, (err) => {
     if (err) throw err;
-    console.log("> Ready on https://localhost:3000");
+    console.log("> Ready on https://127.0.0.1:3000");
   });
 });
