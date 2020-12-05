0f0070b834366d3fe14cfe5bfc65185c9cc41525
Paritosh Batish <batishparitosh2@gmail.com>
2020-12-05 13:14:47 +0530

    Static web app - header

---
+++ static_web_app/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../src/components/Header";
+
+const index = () => {
+  return (
+    <div>
+      <Header />
+      <div></div>
+    </div>
+  );
+};
+
+export default index;
