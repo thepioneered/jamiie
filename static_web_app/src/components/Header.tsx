14cbd09fe62f3e4b7e6d81e23cac69a8c5abce2e
Paritosh Batish <batishparitosh2@gmail.com>
2020-12-10 15:54:27 +0530

    Header done

--- static_web_app/src/components/Header.tsx
+++ static_web_app/src/components/Header.tsx
@@ -3,24 +3,27 @@ import styles from "../../styles/header.module.scss";
 
 const Header = () => {
   return (
-    <div className={styles.container}>
-      <ul className={styles.list}>
-        <li>
-          <button>Jamiie Circle</button>
-        </li>
-        <li>
-          <button>How it works</button>
-        </li>
-        <li>
-          <button>Our story</button>
-        </li>
-        <li>
-          <button>Our team</button>
-        </li>
-        <li>
-          <button>FAQs</button>
-        </li>
-      </ul>
+    <div className={styles.outer__container}>
+      <img className={styles.logo} src="/images/svg/NewLogo.svg" alt="LOgo" />
+      <div className={styles.container}>
+        <ul className={styles.list}>
+          <li>
+            <button>Jamiie Circle</button>
+          </li>
+          <li>
+            <button>How it works</button>
+          </li>
+          <li>
+            <button>Our story</button>
+          </li>
+          <li>
+            <button>Our team</button>
+          </li>
+          <li>
+            <button>FAQs</button>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
